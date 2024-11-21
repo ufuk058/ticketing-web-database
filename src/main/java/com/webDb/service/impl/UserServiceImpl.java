0dd6ed664c759fe678e922a14e139b09444e6474
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-11-21 14:38:11 +0000

    User delete from UI completed by @Where(is_deleted=tru) annotation

--- src/main/java/com/webDb/service/impl/UserServiceImpl.java
+++ src/main/java/com/webDb/service/impl/UserServiceImpl.java
@@ -58,6 +58,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public void delete(String username) {
 
+        User user= userRepository.findByUserName(username);
+        user.setIsDeleted(true);
+        userRepository.save(user);
+
     }
 }
 
