9f661173be186ea52c634220cfe566bc12a2839b
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-11-24 21:45:48 +0000

    Manager Uncompleted and Completed tasks done

--- src/main/java/com/webDb/repository/ProjectRepository.java
+++ src/main/java/com/webDb/repository/ProjectRepository.java
@@ -1,9 +1,16 @@
 package com.webDb.repository;
 
+import com.webDb.dto.ProjectDTO;
+import com.webDb.dto.UserDTO;
 import com.webDb.entity.Project;
+import com.webDb.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {
 
     Project findByProjectCode(String projectCode);
+
+    List<Project> findAllByAssignedManager(User manager);
 }
--- src/main/java/com/webDb/repository/TaskRepository.java
+++ src/main/java/com/webDb/repository/TaskRepository.java
@@ -2,6 +2,12 @@ package com.webDb.repository;
 
 import com.webDb.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
+   @Query("Select Count(t) from Task t where t.project.projectCode = ?1 AND t.taskStatus<>'COMPLETE' ")
+    int totalNonCompletedTask(String projectCode);
+
+   @Query("Select Count(t) from Task t where t.project.projectCode =?1 And t.taskStatus='COMPLETE'")
+    int totalCompletedTask(String projectCode);
 }
